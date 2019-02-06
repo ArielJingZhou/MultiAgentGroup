cba01ca5fcc73986290009c5596ea4e2bd000f7c
ArielJingZhou <45420480+ArielJingZhou@users.noreply.github.com>
2019-02-06 13:57:06 +0000

    add evaluation

--- Problem2_constant_bidding.py
+++ Problem2_constant_bidding.py
@@ -5,29 +5,68 @@ from __future__ import division
 f = open('train.csv', 'r')
 rows = f.readlines()[1:]
 f.close()
-# get pay_price
-pay_price = {}
-for i in rows:
-    cur_advertiser = i.split(',')[23]
-    cur_payprice = i.split(',')[21]
-    if cur_advertiser not in pay_price.keys():
-        pay_price[cur_advertiser] = [int(cur_payprice)]
+# get payprice
+pay_prices = {}
+for row in rows:
+    advertiser = row.split(',')[23]
+    payprice = row.split(',')[21]
+    if advertiser not in pay_prices.keys():
+        pay_prices[advertiser] = [int(payprice)]
     else:
-        pay_price[cur_advertiser].append(int(cur_payprice))
+        pay_prices[advertiser].append(int(payprice))
 # get constant_bid_price: mean of pay_price
 constant_bidprice = {}
-for i in pay_price.keys():
-    constant_bidprice[i] = round(sum(pay_price[i])/len(pay_price[i]))
+for advertiser in pay_prices.keys():
+    constant_bidprice[advertiser] = round(sum(pay_prices[advertiser]) / len(pay_prices[advertiser]))
 
-print(constant_bidprice)
+# evaluate on validation.csv
+f = open('validation.csv', 'r')
+rows_validation = f.readlines()[1:]
+f.close()
+clicks = {}
+winning_impressions = {}
+for row in rows_validation:
+    advertiser = row.split(',')[23]
+    pay_price = row.split(',')[21]
+    if advertiser not in clicks.keys():
+        clicks[advertiser] = 0
+        winning_impressions[advertiser] = 0
+    if constant_bidprice[advertiser] > int(pay_price):
+        winning_impressions[advertiser] += 1
+        if row.split(',')[0] == '1':
+            clicks[advertiser] += 1
+
+click_through_rate = {}
+for advertiser in clicks.keys():
+    click_through_rate[advertiser] = "{:.3%}".format(clicks[advertiser] / winning_impressions[advertiser])
+spend = {}
+for advertiser in clicks.keys():
+    spend[advertiser] = clicks[advertiser] * constant_bidprice[advertiser]
+    if spend[advertiser] > 6250:
+        spend[advertiser] = 6250
+average_cpm = {}
+for advertiser in constant_bidprice.keys():
+    average_cpm[advertiser] = 1000 * constant_bidprice[advertiser]
+average_cpc = constant_bidprice
+
+print('constant_bidprice:\n' + str(constant_bidprice))
+print('\nclicks:\n' + str(clicks))
+print('\nclick_through_rate:\n' + str(click_through_rate))
+print('\nspend:\n' + str(spend))
+print('\naverage_cpm:\n' + str(average_cpm))
+print('\naverage_cpc:\n' + str(average_cpc))
+
+'''
 # save bidprice for test.csv
 f = open('test.csv', 'r')
 rows_test = f.readlines()[1:]
 f.close()
 f = open('testing_bidding_price.csv', 'w')
 f.write('bidid,bidprice\n')
-for i in rows_test:
-    bidid = i.split(',')[2]
-    bidprice = constant_bidprice[i.split(',')[20]]
+for row in rows_test:
+    bidid = row.split(',')[2]
+    bidprice = constant_bidprice[row.split(',')[20]]
     f.write(bidid + ',' + str(bidprice) + '\n')
 f.close()
+'''
+
