eba5a3d0e770a38f88364f886638af2252aaaab0
ArielJingZhou <45420480+ArielJingZhou@users.noreply.github.com>
2019-02-20 14:50:31 +0000

    Update Problem2_constant_bidding.py

--- Problem2_constant_bidding.py
+++ Problem2_constant_bidding.py
@@ -1,52 +1,59 @@
 # int/int -> float
 from __future__ import division
 
-# read file
-f = open('train.csv', 'r')
-rows = f.readlines()[1:]
-f.close()
-# get payprice
-payprices = 0
-for row in rows:
-    payprice = row.split(',')[21]
-    payprices += int(payprice)
 
-# get constant_bid_price: mean of pay_price
-constant_bidprice = payprices / len(rows)
+def getbidprice():
+    # read file
+    f = open('train.csv', 'r')
+    rows = f.readlines()[1:]
+    f.close()
+    # get payprice
+    payprices = 0
+    for row in rows:
+        payprice = row.split(',')[21]
+        payprices += int(payprice)
 
-# evaluate on validation.csv
-f = open('validation.csv', 'r')
-rows_validation = f.readlines()[1:]
-f.close()
-clicks = 0
-winning_impressions = 0
-spend = 0
-for row in rows_validation:
-    payprice = int(row.split(',')[21])
-    if constant_bidprice > payprice:
-        spend += payprice
-        winning_impressions += 1
-        if row.split(',')[0] == '1':
-            clicks += 1
-        if spend > 6250:
-            spend = 6250
-            break
+    # get constant_bidprice: mean of pay_price
+    return payprices / len(rows)
 
-click_through_rate = "{:.3%}".format(clicks / winning_impressions)
 
-if clicks == 0:
-    average_cpm = 0
-    average_cpc = 0
-else:
-    average_cpm = spend / clicks * 1000
-    average_cpc = spend / clicks
+def const(constant_bidprice):
+    # evaluate on validation.csv
+    f = open('validation.csv', 'r')
+    rows_validation = f.readlines()[1:]
+    f.close()
+    clicks = 0
+    winning_impressions = 0
+    spend = 0
+    for row in rows_validation:
+        payprice = int(row.split(',')[21])
+        if constant_bidprice > payprice:
+            spend += payprice / 1000
+            winning_impressions += 1
+            if row.split(',')[0] == '1':
+                clicks += 1
+            if spend > 6250:
+                spend = 6250
+                break
 
-print('constant_bidprice:\n' + str(constant_bidprice))
-print('\nclicks:\n' + str(clicks))
-print('\nclick_through_rate:\n' + str(click_through_rate))
-print('\nspend:\n' + str(spend))
-print('\naverage_cpm:\n' + str(average_cpm))
-print('\naverage_cpc:\n' + str(average_cpc))
+    click_through_rate = "{:.3%}".format(clicks / winning_impressions)
+
+    if clicks == 0:
+        average_cpm = 0
+        average_cpc = 0
+    else:
+        average_cpm = spend / clicks * 1000
+        average_cpc = spend / clicks
+
+    print('constant_bidprice:\n' + str(constant_bidprice))
+    print('\nclicks:\n' + str(clicks))
+    print('\nclick_through_rate:\n' + str(click_through_rate))
+    print('\nspend:\n' + str(spend))
+    print('\naverage_cpm:\n' + str(average_cpm))
+    print('\naverage_cpc:\n' + str(average_cpc))
+
+
+const(getbidprice());
 
 '''
 # save bidprice for test.csv
