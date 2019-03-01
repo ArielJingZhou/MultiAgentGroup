7717206367de244916377d634f3c191c0d69b9d6
ArielJingZhou <45420480+ArielJingZhou@users.noreply.github.com>
2019-03-01 19:40:46 +0000

    + funtion2

--- Problem4/Problem4.py
+++ Problem4/Problem4.py
@@ -10,20 +10,23 @@ train_label = []
 for row in rows_train:
     row_list = row.split(',')
     # weekday, hour, region, city, slotwidth, slotheight, slotformat, slotprice, advertiser
-    train_data.append([row_list[1], row_list[2], row_list[7], row_list[8], row_list[14], row_list[15],
-                       row_list[17], row_list[18], row_list[23]])
+    train_data.append([row_list[1], row_list[2], row_list[7], row_list[8], row_list[14], row_list[15], row_list[17], row_list[18], row_list[23]])
     train_label.append(row_list[0])
 #print(train_label)
 print(train_data[0])
 
 f = open('validation.csv', 'r')
+#f = open('test.csv', 'r')
 rows_validation = f.readlines()[1:]
 f.close()
 test_data = []
+bid_ids = []
 for row in rows_validation:
     row_list = row.split(',')
     test_data.append([row_list[1], row_list[2], row_list[7], row_list[8], row_list[14], row_list[15],
                       row_list[17], row_list[18], row_list[23]])
+    #test_data.append([row_list[0], row_list[1], row_list[6], row_list[7], row_list[13], row_list[14], row_list[16], row_list[17], row_list[20]])
+    #bid_ids.append(row_list[2])
 print(test_data[0])
 
 
@@ -48,23 +51,40 @@ def predict_CTR():
 
 def non_linear_bidding(c, l):
     # evaluate on validation.csv
+    pCTRs = predict_CTR()
+    # tune c and l
+    #f = open("Problem4_tuning_results2.csv", "w")
+    #f.write("constant, lambda, clicks, CTR, spend, CPM, CPC\n")
+    #for c in range(30, 70, 1):
+        #for l in range(20, 50, 1):
     clicks = 0
     winning_impressions = 0
     spend = 0
-    pCTRs = predict_CTR()
+    #l = l * 10 ** (-7)
     bid_prices = (c / l * pCTRs + c * c) ** 0.5 - c
-    print(bid_prices[0])
+    #bid_prices = c * (((pCTRs + (c*c*l*l+pCTRs*pCTRs)**0.5)/c/l) ** (1.0/3)-(c*l/(pCTRs + (c*c*l*l+pCTRs*pCTRs)**0.5))**(1.0/3))
+
+    '''
+    f = open('testing_bidding_price.csv', 'w')
+    f.write('bidid,bidprice\n')
+    for i in range(len(bid_ids)):
+        bid_id = bid_ids[i]
+        bid_price = bid_prices[i]
+        print(bid_prices[i])
+        f.write(bid_id + ',' + str(bid_price) + '\n')
+    f.close()
+    '''
     i = 0
     for row in rows_validation:
         pay_price = int(row.split(',')[21])
         bid_price = bid_prices[i]
         if bid_price >= pay_price:
-            spend += pay_price
+            spend += pay_price / 1000
             winning_impressions += 1
             if row.split(',')[0] == '1':
                 clicks += 1
-            if spend > 6250000:
-                spend = 6250000
+            if spend > 6250:
+                spend = 6250
                 break
         i += 1
 
@@ -81,24 +101,26 @@ def non_linear_bidding(c, l):
     print('\nspend:' + str(spend))
     print('\naverage_cpm:' + str(average_cpm))
     print('\naverage_cpc:' + str(average_cpc))
-    return str(c) + "," + str(l) + "," + str(clicks) + "," + str(click_through_rate) + "," + str(spend/1000) + "," \
-           + str(average_cpm) + "," + str(average_cpc)
+            #f.write(str(c) + "," + str(l) + "," + str(clicks) + "," + str(click_through_rate) + "," + str(spend/1000) + ","  + str(average_cpm) + "," + str(average_cpc) + "\n")
 
+    return str(c) + "," + str(l) + "," + str(clicks) + "," + str(click_through_rate) + "," + str(spend) + "," \
+                   + str(average_cpm) + "," + str(average_cpc)
+
+
+non_linear_bidding(80, 25 * 10 ** (-7))
 
 '''
-tuning result: c = 80, l = 2.5 * 10 ** (-6) best
+tuning result: 
+function 1:
+c = 80, l = 2.5 * 10 ** (-6) best
 clicks	CTR	    spend	CPM	        CPC
 90	    0.06%	6250	41.82084619	69.44444444
 
-f = open("Problem4_tuning_results.csv", "w")
-f.write("constant, lambda, clicks, CTR, spend, CPM, CPC\n")
-for c in range(10, 85, 5):
-    for l in range(1, 30, 1):
-        print("\n\nc: " + str(c) + "l: " + str(l))
-        f.write(non_linear_bidding(c, l * 10 ** (-7)) + "\n")
-f.close()
+function 2:
+constant	lambda	    clicks	 CTR	 spend	 CPM	        CPC
+60	        3.30E-06	86	     0.06%	 6250	 41541.21221	72674.4186
 
 '''
-non_linear_bidding(80, 25 * 10 ** (-7))
+
 
 
