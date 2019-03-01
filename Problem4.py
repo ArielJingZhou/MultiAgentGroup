31b07ac19b0d5c7058878518f4a20ae0f48e9a08
ArielJingZhou <45420480+ArielJingZhou@users.noreply.github.com>
2019-03-01 14:13:20 +0000

    Tuning constant and lambda

--- Problem4.py
+++ Problem4.py
@@ -27,44 +27,6 @@ for row in rows_validation:
 print(test_data[0])
 
 
-def non_linear_bidding(c, l):
-    # evaluate on validation.csv
-    clicks = 0
-    winning_impressions = 0
-    spend = 0
-    pCTRs = predict_CTR()
-    bid_prices = (c / l * pCTRs + c * c) ** 0.5 - c
-    print(bid_prices[0])
-    i = 0
-    for row in rows_validation:
-        pay_price = int(row.split(',')[21])
-        bid_price = bid_prices[i]
-        if bid_price >= pay_price:
-            spend += pay_price
-            winning_impressions += 1
-            if row.split(',')[0] == '1':
-                clicks += 1
-            if spend > 6250000:
-                spend = 6250
-                break
-        i += 1
-
-    click_through_rate = "{:.3%}".format(clicks / winning_impressions)
-
-    if clicks == 0:
-        average_cpm = 0
-        average_cpc = 0
-    else:
-        average_cpm = spend / winning_impressions * 1000
-        average_cpc = spend / clicks
-
-    print('\nclicks:\n' + str(clicks))
-    print('\nclick_through_rate:\n' + str(click_through_rate))
-    print('\nspend:\n' + str(spend))
-    print('\naverage_cpm:\n' + str(average_cpm))
-    print('\naverage_cpc:\n' + str(average_cpc))
-
-
 def predict_CTR():
     # initialize Pool
     train_pool = Pool(train_data,
@@ -84,4 +46,59 @@ def predict_CTR():
     return preds
 
 
-non_linear_bidding(20, 5.2 * 10 ** (-7))
+def non_linear_bidding(c, l):
+    # evaluate on validation.csv
+    clicks = 0
+    winning_impressions = 0
+    spend = 0
+    pCTRs = predict_CTR()
+    bid_prices = (c / l * pCTRs + c * c) ** 0.5 - c
+    print(bid_prices[0])
+    i = 0
+    for row in rows_validation:
+        pay_price = int(row.split(',')[21])
+        bid_price = bid_prices[i]
+        if bid_price >= pay_price:
+            spend += pay_price
+            winning_impressions += 1
+            if row.split(',')[0] == '1':
+                clicks += 1
+            if spend > 6250000:
+                spend = 6250000
+                break
+        i += 1
+
+    click_through_rate = "{:.3%}".format(clicks / winning_impressions)
+
+    if clicks == 0:
+        average_cpm = 0
+        average_cpc = 0
+    else:
+        average_cpm = spend / winning_impressions * 1000
+        average_cpc = spend / clicks
+    print('\nclicks:' + str(clicks))
+    print('\nclick_through_rate:' + str(click_through_rate))
+    print('\nspend:' + str(spend))
+    print('\naverage_cpm:' + str(average_cpm))
+    print('\naverage_cpc:' + str(average_cpc))
+    return str(c) + "," + str(l) + "," + str(clicks) + "," + str(click_through_rate) + "," + str(spend/1000) + "," \
+           + str(average_cpm) + "," + str(average_cpc)
+
+
+'''
+tuning result: c = 80, l = 2.5 * 10 ** (-6) best
+clicks	CTR	    spend	CPM	        CPC
+90	    0.06%	6250	41.82084619	69.44444444
+
+f = open("Problem4_tuning_results.csv", "w")
+f.write("constant, lambda, clicks, CTR, spend, CPM, CPC\n")
+for c in range(10, 85, 5):
+    for l in range(1, 30, 1):
+        print("\n\nc: " + str(c) + "l: " + str(l))
+        f.write(non_linear_bidding(c, l * 10 ** (-7)) + "\n")
+f.close()
+
+'''
+non_linear_bidding(80, 25 * 10 ** (-7))
+
+
